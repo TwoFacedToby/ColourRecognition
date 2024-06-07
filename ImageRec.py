d4594d09bc45b8e14b77ea3154aad04a202eac32
Tobias Pedersen Gørlyk <tobiasgoerlyk@hotmail.com>
2024-06-07 12:12:41 +0200

    changed walls and black colors

--- ImageRec.py
+++ ImageRec.py
@@ -3,8 +3,10 @@ import cv2
 from enum import Enum
 
 # Capturing video through webcam
-# cam = cv2.VideoCapture(0)
-cam = cv2.VideoCapture("TrackVideos/Position1_normal.mp4")
+cam = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+
+
+# cam = cv2.VideoCapture("TrackVideos/Position1_normal.mp4")
 
 
 class State:
@@ -33,7 +35,7 @@ class Robot:
 class Type(Enum):
     Robot = [255, 255, 0]
     Wall = [0, 0, 0]
-    Ball = [255, 255, 0]
+    Ball = [200, 200, 0]
     OrangeBall = [200, 255, 0]
     SmallBlueGoal = [255, 101, 0]
     BigGreenGoal = [30, 170, 70]
@@ -47,13 +49,15 @@ class Color(Enum):
     BROWN = 3
     GREEN = 4
     BLUE = 5
-    Yellow = 6
+    YELLOW = 6
+    BLACK = 7
 
 
 ballsState = []
 wallsState = []
 smallGoalPos = []
 bigGoalPos = []
+white_egg = []
 robot_state = Robot([0, 0], [0, 0], [0, 0])
 walls = []
 
@@ -64,18 +68,19 @@ def obstacle_lines(image):
     # perform edge detection
     edges = cv2.Canny(grayscale, 20, 100)
     # detect lines in the image using hough lines technique
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 60, np.array([]), 50, 5)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 30, np.array([]), 20, 0.1)
     # iterate over the output lines and draw them
-    if len(walls) < 100:
+    if len(walls) < 50:
         walls.append(lines)
     else:
         walls.pop(0)
         walls.append(lines)
     for wall in walls:
-        for line in wall:
-            for x1, y1, x2, y2 in line:
-                cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                cv2.line(edges, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        if wall is not None:
+            for line in wall:
+                for x1, y1, x2, y2 in line:
+                    cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                    cv2.line(edges, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
 
 def recognise_state_and_draw(image, mask, types):
@@ -89,7 +94,7 @@ def recognise_state_and_draw(image, mask, types):
         match types:
             case Type.Wall:
                 if 10 < area < 300:
-                    show = "square"
+                    show = "none"
 
             case Type.SmallBlueGoal:
                 if 100 < area < 1500:
@@ -98,15 +103,19 @@ def recognise_state_and_draw(image, mask, types):
                     show = "square"
 
             case Type.BigGreenGoal:
-                if 100 < area < 1500:
+                if 20 < area < 1500:
                     bigGoalPos.clear()
                     bigGoalPos.append([x, y])
                     show = "square"
 
             case Type.Ball:
-                if 120 < area < 1500:
+                if 120 < area < 500:
                     ballsState.append(Ball(x=x, y=y, is_orange=False))
                     show = "circle"
+                elif 500 < area < 2500:
+                    global white_egg
+                    white_egg = [x, y]
+                    show = "square"
 
             case Type.OrangeBall:
                 if 120 < area < 1500:
@@ -114,16 +123,18 @@ def recognise_state_and_draw(image, mask, types):
                     show = "circle"
 
             case Type.Robot:
-                if 120 < area < 1500:
+                if 400 < area < 1500 and h/2 < w < h*2 and w/2 < h < w*2:
                     if robot_state.pos_1 == [0, 0]:
                         robot_state.pos_1 = [x, y]
                     elif robot_state.pos_2 == [0, 0]:
                         robot_state.pos_2 = [x, y]
                     elif robot_state.pos_3 == [0, 0]:
                         robot_state.pos_3 = [x, y]
+                        print(robot_state.pos_1, " ", robot_state.pos_2, " ", robot_state.pos_3)
+
                     show = "square"
         if show == "circle":
-            cv2.circle(image, (x + 15, y + 15), 15, (color[0], color[1], color[2]), 2)
+            cv2.circle(image, (x + 8, y + 8), 8, (color[0], color[1], color[2]), 2)
         elif show == "square":
             cv2.rectangle(image, (x, y), (x + w, y + h), (color[0], color[1], color[2]), 2)
 
@@ -139,20 +150,23 @@ def mask_from_color(color, image_hsv, clean_image):
             color_low = np.array([10, 80, 180], np.uint8)
             color_high = np.array([14, 170, 200], np.uint8)
         case Color.RED:
-            color_low = np.array([0, 50, 20], np.uint8)
-            color_high = np.array([20, 100, 200], np.uint8)
+            color_low = np.array([10, 70, 20], np.uint8)
+            color_high = np.array([20, 100, 160], np.uint8)
         case Color.BROWN:
-            color_low = np.array([10, 30, 20], np.uint8)
-            color_high = np.array([60, 100, 140], np.uint8)
+            color_low = np.array([0, 0, 0], np.uint8)
+            color_high = np.array([255, 255, 255], np.uint8)
+        case Color.YELLOW:
+            color_low = np.array([0, 0, 0], np.uint8)
+            color_high = np.array([255, 255, 255], np.uint8)
         case Color.GREEN:
-            color_low = np.array([25, 50, 70], np.uint8)
-            color_high = np.array([50, 200, 200], np.uint8)
+            color_low = np.array([70, 30, 70], np.uint8)
+            color_high = np.array([80, 50, 110], np.uint8)
         case Color.BLUE:
             color_low = np.array([100, 100, 100], np.uint8)
             color_high = np.array([130, 200, 200], np.uint8)
-        case Color.Yellow:
-            color_low = np.array([200, 177, 123], np.uint8)
-            color_high = np.array([173, 146, 79], np.uint8)
+        case Color.BLACK:
+            color_low = np.array([0, 45, 90], np.uint8)
+            color_high = np.array([20, 70, 110], np.uint8)
     return cv2.dilate(cv2.inRange(image_hsv, color_low, color_high), clean_image)
 
 
@@ -161,9 +175,9 @@ def get_types():
             Type.OrangeBall,
             Type.Wall,
             Type.Wall,
+            Type.Robot,
             Type.BigGreenGoal,
-            Type.SmallBlueGoal,
-            Type.Robot]
+            Type.SmallBlueGoal]
 
 
 def get_masks(image_hsv):
@@ -172,9 +186,9 @@ def get_masks(image_hsv):
             mask_from_color(Color.ORANGE, image_hsv, clean_image),
             mask_from_color(Color.RED, image_hsv, clean_image),
             mask_from_color(Color.BROWN, image_hsv, clean_image),
+            mask_from_color(Color.BLACK, image_hsv, clean_image),
             mask_from_color(Color.GREEN, image_hsv, clean_image),
-            mask_from_color(Color.BLUE, image_hsv, clean_image),
-            mask_from_color(Color.Yellow, image_hsv, clean_image)]
+            mask_from_color(Color.BLUE, image_hsv, clean_image)]
 
 
 def reset():
@@ -197,9 +211,9 @@ def render():
     for i in range(len(masks)):
         recognise_state_and_draw(image, masks[i], types[i])
     obstacle_lines(image)
-    draw_robot(image)
+    #draw_robot(image)
 
-    cv2.imshow("Multiple Color Detection in Real-TIme", image)
+    cv2.imshow("Multiple Color Detection in Real-Time", image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
     return State(
